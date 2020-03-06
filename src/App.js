bf5a9f75df80bfcd9dcecbfe1f040e99e4225899
Melissa Martinez <58582650+melissmartinez@users.noreply.github.com>
2020-03-05 18:33:52 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BoxGenerator from './components/boxgenerator';
+
+const App = () => {
+  return (
+    <div className="App ">
+      <BoxGenerator />
+    </div>
+  );
+}
+
+export default App;
